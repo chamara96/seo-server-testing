ed1307fecbaaf9086c1f7243cdf920ff0713c872
Chamara Madhushan <cmb.info96@gmail.com>
2020-06-13 02:36:19 +0530

    Final Index v1

--- SEO-final/assets/js/owl-custom.js
+++ SEO-final/assets/js/owl-custom.js
@@ -26,33 +26,33 @@ $(document).ready(function () {
 
 
     var slides = [
-        {title : 'Best Deals', intro : 'Find best offers, deals, discount vouchers and stores near you'},
-        {title: 'Welcome Gift', intro : 'Register and get Rs 5,000 Unconditional Cash Voucher Instantly'},
-        {title: 'Earn Money', intro : 'Like, comment & share on social media and EARN UNLIMITED MONEY'},
-        {title: 'Play with new Technology', intro : 'Have Fun with Augmented Reality (AR), unlock special vouchers & offers with AR'},
-        {title: 'Win Spot Deals', intro : 'Stand a chance to win Mega Spot Deals with only Rs.10'},
+        { title: 'Best Deals', intro: 'Find best offers, deals, discount vouchers and stores near you' },
+        { title: 'Welcome Gift', intro: 'Register and get Rs 5,000 Unconditional Cash Voucher Instantly' },
+        { title: 'Earn Money', intro: 'Like, comment & share on social media and EARN UNLIMITED MONEY' },
+        { title: 'Play with new Technology', intro: 'Have Fun with Augmented Reality (AR), unlock special vouchers & offers with AR' },
+        { title: 'Win Spot Deals', intro: 'Stand a chance to win Mega Spot Deals with only Rs.10' },
     ]
 
     let i = 1
     owl.on('changed.owl.carousel', function (event) {
-        if(i==5) i = 0;
+        if (i == 5) i = 0;
 
         var html = $('<div> \n\
-        <h2 class="title" style="font-weight: bold; font-size: 50px">'+slides[i].title+'</h2> \n\
-        <p class="intro">'+slides[i].intro+' \n\
+        <h2 class="title" style="font-weight: bold; font-size: 50px">'+ slides[i].title + '</h2> \n\
+        <p class="intro">'+ slides[i].intro + ' \n\
         </p> \n\
      </div>');
 
-     
-                 
-            
-  /*$('.side-slideshow').slideUp(300, function() {
-    $( this ).remove();
-  });*/ 
-  $('#side-slideshow').empty();          
-  $('#side-slideshow').append(html);
-  i++;
-  // html.slideDown();
+
+
+
+        /*$('.side-slideshow').slideUp(300, function() {
+          $( this ).remove();
+        });*/
+        $('#side-slideshow').empty();
+        $('#side-slideshow').append(html);
+        i++;
+        // html.slideDown();
     })
 
 
--- SEO-final/js/app-functions.js
+++ SEO-final/js/app-functions.js
@@ -1,312 +1,3 @@
-//Canvsa Function
-// function canvasSupport() {
-//     return Modernizr.canvas;
-// }
-
-// function canvasApp() {
-//     if (!canvasSupport()) {
-//         return;
-//     }
-
-//     var theCanvas = document.getElementById("canvasOne");
-//     var context = theCanvas.getContext("2d");
-
-//     var displayWidth;
-//     var displayHeight;
-//     var timer;
-//     var wait;
-//     var count;
-//     var numToAddEachFrame;
-//     var particleList;
-//     var recycleBin;
-//     var particleAlpha;
-//     var r, g, b;
-//     var fLen;
-//     var m;
-//     var projCenterX;
-//     var projCenterY;
-//     var zMax;
-//     var turnAngle;
-//     var turnSpeed;
-//     var sphereRad, sphereCenterX, sphereCenterY, sphereCenterZ;
-//     var particleRad;
-//     var zeroAlphaDepth;
-//     var randAccelX, randAccelY, randAccelZ;
-//     var gravity;
-//     var rgbString;
-//     //we are defining a lot of variables used in the screen update functions globally so that they don't have to be redefined every frame.
-//     var p;
-//     var outsideTest;
-//     var nextParticle;
-//     var sinAngle;
-//     var cosAngle;
-//     var rotX, rotZ;
-//     var depthAlphaFactor;
-//     var i;
-//     var theta, phi;
-//     var x0, y0, z0;
-
-//     init();
-
-//     function init() {
-//         wait = 1;
-//         count = wait - 1;
-//         numToAddEachFrame = 8;
-
-//         //particle color
-//         r = 255;//70
-//         g = 255;//255
-//         b = 255;//140
-
-//         rgbString = "rgba(" + r + "," + g + "," + b + ","; //partial string for color which will be completed by appending alpha value.
-//         particleAlpha = 1; //maximum alpha
-
-//         displayWidth = theCanvas.width;
-//         displayHeight = theCanvas.height;
-
-//         fLen = 320; //represents the distance from the viewer to z=0 depth.
-
-//         //projection center coordinates sets location of origin
-//         projCenterX = displayWidth / 2;
-//         projCenterY = displayHeight / 2;
-
-//         //we will not draw coordinates if they have too large of a z-coordinate (which means they are very close to the observer).
-//         zMax = fLen - 2;
-
-//         particleList = {};
-//         recycleBin = {};
-
-//         //random acceleration factors - causes some random motion
-//         randAccelX = 0.1;
-//         randAccelY = 0.1;
-//         randAccelZ = 0.1;
-
-//         gravity = 0; //try changing to a positive number (not too large, for example 0.3), or negative for floating upwards.
-
-//         particleRad = 2.5;
-//         sphereRad = 280;
-//         sphereCenterX = 0;
-//         sphereCenterY = 0;
-//         sphereCenterZ = -3 - sphereRad;
-
-//         //alpha values will lessen as particles move further back, causing depth-based darkening:
-//         zeroAlphaDepth = -750;
-
-//         turnSpeed = 2 * Math.PI / 1600; //the sphere will rotate at this speed (one complete rotation every 1600 frames).
-//         turnAngle = 0; //initial angle
-
-//         timer = setInterval(onTimer, 1000 / 24);
-//     }
-
-//     function onTimer() {
-//         //if enough time has elapsed, we will add new particles.		
-//         count++;
-//         if (count >= wait) {
-
-//             count = 0;
-//             for (i = 0; i < numToAddEachFrame; i++) {
-//                 theta = Math.random() * 2 * Math.PI;
-//                 phi = Math.acos(Math.random() * 2 - 1);
-//                 x0 = sphereRad * Math.sin(phi) * Math.cos(theta);
-//                 y0 = sphereRad * Math.sin(phi) * Math.sin(theta);
-//                 z0 = sphereRad * Math.cos(phi);
-
-//                 //We use the addParticle function to add a new particle. The parameters set the position and velocity components.
-//                 //Note that the velocity parameters will cause the particle to initially fly outwards away from the sphere center (after
-//                 //it becomes unstuck).
-//                 var p = addParticle(x0, sphereCenterY + y0, sphereCenterZ + z0, 0.002 * x0, 0.002 * y0, 0.002 * z0);
-
-//                 //we set some "envelope" parameters which will control the evolving alpha of the particles.
-//                 p.attack = 50;
-//                 p.hold = 50;
-//                 p.decay = 160;
-//                 p.initValue = 0;
-//                 p.holdValue = particleAlpha;
-//                 p.lastValue = 0;
-
-//                 //the particle will be stuck in one place until this time has elapsed:
-//                 p.stuckTime = 80 + Math.random() * 20;
-
-//                 p.accelX = 0;
-//                 p.accelY = gravity;
-//                 p.accelZ = 0;
-//             }
-//         }
-
-//         //update viewing angle
-//         turnAngle = (turnAngle + turnSpeed) % (2 * Math.PI);
-//         sinAngle = Math.sin(turnAngle);
-//         cosAngle = Math.cos(turnAngle);
-
-//         //background fill
-//         context.fillStyle = "#000000";
-//         context.clearRect(0, 0, displayWidth, displayHeight);
-//         // context.fillRect(0, 0, displayWidth, displayHeight);
-
-//         //update and draw particles
-//         p = particleList.first;
-//         while (p != null) {
-//             //before list is altered record next particle
-//             nextParticle = p.next;
-
-//             //update age
-//             p.age++;
-
-//             //if the particle is past its "stuck" time, it will begin to move.
-//             if (p.age > p.stuckTime) {
-//                 p.velX += p.accelX + randAccelX * (Math.random() * 2 - 1);
-//                 p.velY += p.accelY + randAccelY * (Math.random() * 2 - 1);
-//                 p.velZ += p.accelZ + randAccelZ * (Math.random() * 2 - 1);
-
-//                 p.x += p.velX;
-//                 p.y += p.velY;
-//                 p.z += p.velZ;
-//             }
-
-//             /*
-//             We are doing two things here to calculate display coordinates.
-//             The whole display is being rotated around a vertical axis, so we first calculate rotated coordinates for
-//             x and z (but the y coordinate will not change).
-//             Then, we take the new coordinates (rotX, y, rotZ), and project these onto the 2D view plane.
-//             */
-//             rotX = cosAngle * p.x + sinAngle * (p.z - sphereCenterZ);
-//             rotZ = -sinAngle * p.x + cosAngle * (p.z - sphereCenterZ) + sphereCenterZ;
-//             m = fLen / (fLen - rotZ);
-//             p.projX = rotX * m + projCenterX;
-//             p.projY = p.y * m + projCenterY;
-
-//             //update alpha according to envelope parameters.
-//             if (p.age < p.attack + p.hold + p.decay) {
-//                 if (p.age < p.attack) {
-//                     p.alpha = (p.holdValue - p.initValue) / p.attack * p.age + p.initValue;
-//                 }
-//                 else if (p.age < p.attack + p.hold) {
-//                     p.alpha = p.holdValue;
-//                 }
-//                 else if (p.age < p.attack + p.hold + p.decay) {
-//                     p.alpha = (p.lastValue - p.holdValue) / p.decay * (p.age - p.attack - p.hold) + p.holdValue;
-//                 }
-//             }
-//             else {
-//                 p.dead = true;
-//             }
-
-//             //see if the particle is still within the viewable range.
-//             if ((p.projX > displayWidth) || (p.projX < 0) || (p.projY < 0) || (p.projY > displayHeight) || (rotZ > zMax)) {
-//                 outsideTest = true;
-//             }
-//             else {
-//                 outsideTest = false;
-//             }
-
-//             if (outsideTest || p.dead) {
-//                 recycle(p);
-//             }
-
-//             else {
-//                 //depth-dependent darkening
-//                 depthAlphaFactor = (1 - rotZ / zeroAlphaDepth);
-//                 depthAlphaFactor = (depthAlphaFactor > 1) ? 1 : ((depthAlphaFactor < 0) ? 0 : depthAlphaFactor);
-//                 context.fillStyle = rgbString + depthAlphaFactor * p.alpha + ")";
-
-//                 //draw
-//                 context.beginPath();
-//                 context.arc(p.projX, p.projY, m * particleRad, 0, 2 * Math.PI, false);
-//                 context.closePath();
-//                 context.fill();
-//             }
-
-//             p = nextParticle;
-//         }
-//     }
-
-//     function addParticle(x0, y0, z0, vx0, vy0, vz0) {
-//         var newParticle;
-//         var color;
-
-//         //check recycle bin for available drop:
-//         if (recycleBin.first != null) {
-//             newParticle = recycleBin.first;
-//             //remove from bin
-//             if (newParticle.next != null) {
-//                 recycleBin.first = newParticle.next;
-//                 newParticle.next.prev = null;
-//             }
-//             else {
-//                 recycleBin.first = null;
-//             }
-//         }
-//         //if the recycle bin is empty, create a new particle (a new ampty object):
-//         else {
-//             newParticle = {};
-//         }
-
-//         //add to beginning of particle list
-//         if (particleList.first == null) {
-//             particleList.first = newParticle;
-//             newParticle.prev = null;
-//             newParticle.next = null;
-//         }
-//         else {
-//             newParticle.next = particleList.first;
-//             particleList.first.prev = newParticle;
-//             particleList.first = newParticle;
-//             newParticle.prev = null;
-//         }
-
-//         //initialize
-//         newParticle.x = x0;
-//         newParticle.y = y0;
-//         newParticle.z = z0;
-//         newParticle.velX = vx0;
-//         newParticle.velY = vy0;
-//         newParticle.velZ = vz0;
-//         newParticle.age = 0;
-//         newParticle.dead = false;
-//         if (Math.random() < 0.5) {
-//             newParticle.right = true;
-//         }
-//         else {
-//             newParticle.right = false;
-//         }
-//         return newParticle;
-//     }
-
-//     function recycle(p) {
-//         //remove from particleList
-//         if (particleList.first == p) {
-//             if (p.next != null) {
-//                 p.next.prev = null;
-//                 particleList.first = p.next;
-//             }
-//             else {
-//                 particleList.first = null;
-//             }
-//         }
-//         else {
-//             if (p.next == null) {
-//                 p.prev.next = null;
-//             }
-//             else {
-//                 p.prev.next = p.next;
-//                 p.next.prev = p.prev;
-//             }
-//         }
-//         //add to recycle bin
-//         if (recycleBin.first == null) {
-//             recycleBin.first = p;
-//             p.prev = null;
-//             p.next = null;
-//         }
-//         else {
-//             p.next = recycleBin.first;
-//             recycleBin.first.prev = p;
-//             recycleBin.first = p;
-//             p.prev = null;
-//         }
-//     }
-// }
-
 function svgdrawing() {
     var path = document.getElementById('ewrcpck2x369');
     var length = path.getTotalLength();
@@ -316,7 +7,7 @@ function svgdrawing() {
         'none';
 
     // path.style.stroke='white';
-    path.style.stroke='#c30c15';
+    path.style.stroke = '#c30c15';
 
     // Set up the starting positions
     path.style.strokeDasharray = length + ' ' + length;
@@ -334,30 +25,242 @@ function svgdrawing() {
     path.style.strokeDashoffset = '0';
 }
 
-function splashtag_anim() {
-
-    var
-        easer1 = "Circ.easeInOut",
-        easer2 = "Quad.easeInOut",
-        easer3 = "Circ.easeIn",
-        easer4 = "Circ.easeOut",
-        easer5 = "Quad.easeOut";
-
-    var splashtag = document.getElementById('splashtag');
+// ====================================================
 
 
-    var st_gs = new SplitText(splashtag, { type: "words,chars" }),
-        st_chars = st_gs.chars,
-        st_l = st_gs.chars.length;
-
-    for (i = 0; i < st_l / 2; i++) {
-        TweenMax.from(splashtag, 1.5, { autoAlpha: 1, ease: easer5 });
-        TweenMax.from([st_chars[Math.floor(st_l / 2) + i], st_chars[Math.floor(st_l / 2) - i]], 1, { y: 100, opacity: 0, ease: Back.easeOut, delay: i * 0.2 });
-    };
-    $init_done = "true";
-    // initlogoscale();
+//start copy from spot.js 
+function setSpotOwl() {
+    var owl = $('#spot-list');
+    owl.owlCarousel({
+        items: 4,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
+        itemsMobile: [479, 1],
+        loop: true,
+        responsiveRefreshRate: 0,
+        autoplay: true,
+        autoPlay: 3000,
+        smartSpeed: 1000,
+        nav: true,
+        stopOnHover: false,
+        /*navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],*/
+        responsive: {
+            0: {
+                items: 1,
+            },
+            582: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            998: {
+                items: 4,
+            }
+        }
+    });
 }
 
-// function resize() {
-//     $("#canvasOne").outerWidth($(window).width() - $("#canvasOne").offset().top - Math.abs($("#canvasOne").outerWidth(true) - $("#canvasOne").outerWidth()));
-// }
+function setIndustryOwl() {
+    var owl = $('#indus-list');
+    owl.owlCarousel({
+        items: 4,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
+        itemsMobile: [479, 1],
+        loop: true,
+        responsiveRefreshRate: 0,
+        autoplay: true,
+        autoPlay: 3000,
+        smartSpeed: 1000,
+        nav: true,
+        stopOnHover: false,
+        /*navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],*/
+        responsive: {
+            0: {
+                items: 1,
+            },
+            582: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            998: {
+                items: 4,
+            }
+        }
+    });
+}
+//end copy from spot.js 
+
+// ====================================================
+
+//Start copy from js/client.js
+
+var merchants = [];
+var blogs = [];
+destroySlick();
+setSlick();
+
+
+function setSlick() {
+
+
+    $("#client-list").empty();
+    var html = '';
+    for (let i = 0; i < merchants.length; i++) {
+
+        /*html = `<a href="${merchants[i].web_url}" class="slide"><img src="http://blog.chamaralabs.com/${merchants[i].image_url}"></a>`;*/
+        html = `<div class="slide cont" style="padding: 5px 10px; vertical-align:top"><a target="_blank" rel="noopener noreferrer" href="${merchants[i].web_url}" > <div class="port wow fadeIn">
+                 <img id="c_img"  src="http://blog.chamaralabs.com/${merchants[i].image_url}" class="img-fluid" style="width:100%;"/>
+                <div class="work-text-content">
+                  <h3 style="color:#fff">${merchants[i].title}</h3>
+                </div>
+
+              </div> </a></div>`;
+        $("#client-list").append(html);
+    }
+
+    $('.customer-logos').slick({
+        rows: 3,
+        slidesPerRow: 5,
+        /*slidesToShow: 5,
+        slidesToScroll: 5,*/
+        autoplay: true,
+        autoplaySpeed: 3000,
+        nextArrow: '<button class="slick-prev"><span><</span></button>',
+        prevArrow: '<button class="slick-next"><span>></span></button>',
+        arrows: true,
+        dots: true,
+        cssEase: "ease",
+        pauseOnHover: false,
+        pauseOnClick: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                rows: 3,
+                slidesPerRow: 3,
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                rows: 3,
+                slidesPerRow: 1,
+            }
+        }]
+    });
+
+
+
+
+}
+
+function destroySlick() {
+    if ($('.customer-logos').hasClass('slick-initialized')) {
+        $('.customer-logos').slick('destroy');
+    }
+}
+
+function setBlogs(blogs) {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    $("#blog-list").empty();
+    var html = '';
+
+    for (let j = 0; j < blogs.length; j++) {
+
+        /*var name = blogs[j].name.split(' ')
+        console.log(name)
+        html = `<div class="col-lg-4 col-md-12">
+    <div class="tw-latest-post" style="text-align:left">
+        <div class="latest-post-media text-center">
+            <img src="http://blog.chamaralabs.com/${blogs[j].featured_image}" alt="blog_image_one" class="img-fluid" style="height: 200px">
+        </div>
+        <!-- End Latest Post Media -->
+        <div class="post-body" style="height: 500px; overflow-y : scroll">
+            <div class="post-item-date">
+                <div class="post-date">
+                    <span class="date">${blogs[j].published_at.substring(8,10)}</span>
+                    <span class="month">${monthNames[blogs[j].published_at.substring(5,7) - 1]}</span>
+
+                
+                </div>
+            </div>
+            <!-- End Post Item Date -->
+            <div class="post-info">
+                <div class="post-meta">
+                    <span class="post-author">
+                        Posted by <a href="#">${blogs[j].created_by_name.split(' ')[0]+ blogs[j].created_by_name.split(' ')[1] }</a>
+                    </span>
+                </div>
+                <!-- End Post Meta -->
+                <h3 class="post-title"><a href="#">${blogs[j].name}</a></h3>
+                <div class="entry-content">
+                    <p>
+                    ${blogs[j].intro}
+                    </p>
+                </div>
+                <!-- End Entry Content -->
+                <a href="#" class="tw-readmore">Read More
+                    <!--i class="fa fa-angle-right"></i-->
+                </a>
+            </div>
+            <!-- End Post info -->
+        </div>
+        <!-- End Post Body -->
+    </div>
+    <!-- End Tw Latest Post -->
+</div>
+<!-- End Col -->`;*/
+
+
+        html = `<div class="post item">
+        <div class="post-img"> <img class="img-responsive" src="http://blog.chamaralabs.com/${blogs[j].featured_image}" alt=""/> </div>
+        <div class="post-info" style="margin-left:0">
+          <h3><a href="#">${blogs[j].name}</a></h3>
+          <h6>${monthNames[blogs[j].published_at.substring(5, 7) - 1]} ${blogs[j].published_at.substring(8, 10)}, ${blogs[j].published_at.substring(0, 4)}</h6>
+          <p>${blogs[j].intro}</p>
+          <a class="readmore" href="http://blog.chamaralabs.com/article/posts/${blogs[j].encode_id}/${blogs[j].slug}"><span>Read More</span></a> </div>
+      </div>`;
+        /*$("#blog-list").append(html);*/
+        $("#blog-list").append(html);
+    }
+
+
+
+}
+
+function setOwl() {
+    var owl = $('#blog-list');
+    owl.owlCarousel({
+        items: 3,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
+        itemsMobile: [479, 1],
+        loop: true,
+        responsiveRefreshRate: 0,
+        autoplay: true,
+        autoPlay: 3000,
+        smartSpeed: 400,
+        nav: true,
+        stopOnHover: false,
+        /*navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],*/
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            998: {
+                items: 3,
+            }
+        }
+    });
+}
+//End copy from js/client.js
